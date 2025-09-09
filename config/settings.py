89b38a87b163da4ff7563d14cfdd7d658a189e48
Nick Greenfield <nagreenfield@yahoo.com>
2025-09-09 13:24:32 -0700

    Procfile

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-qp#92z!!+o07b!+=-5)!pp5cdzz9=7_!-%ztj(j&teq3rc2r02
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['football-pickem-league-81e442b59816.herokuapp.com']
+ALLOWED_HOSTS = ['football-pickem-league-81e442b59816.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -118,7 +119,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
